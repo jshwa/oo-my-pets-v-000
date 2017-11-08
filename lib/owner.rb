fdac6c61a417d5683beeebcdc2d421ad79f16976
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 02:33:34 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,21 @@
 class Owner
   attr_accessor :pets
+  @@all = []
 
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
+    self.all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def reset_all
+    @@all.clear
+  end
+
+  
 
   def buy_fish(name)
     fish = Fish.new(name)
