3102eda19db4e9c2656c77de65178541194e11aa
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 02:40:29 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,20 +1,29 @@
 class Owner
   attr_accessor :pets
+  attr_reader :species
   @@all = []
 
   def initialize
-    
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = "human"
+    @@all << self
   end
 
   def self.all
     @@all
   end
 
+  def self.count
+    @@all.length
+  end
+
   def reset_all
     @@all.clear
   end
 
-
+  def say_species
+    @species
+  end
 
   def buy_fish(name)
     fish = Fish.new(name)
