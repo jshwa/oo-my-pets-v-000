5c3c4adb25be6ae0cf8cadd8297a2a2dba1c2f3f
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 02:54:58 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,4 +29,15 @@ class Owner
     fish = Fish.new(name)
     @pets[:fishes]<< fish
   end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cat]<< cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dog]<< dog
+  end
+  
 end
